f8a5f3f86fe165b5743acc9d9396a19ce03bc89f
Jennifer Kaplan <jenniferkaplan.nyc@gmail.com>
2017-01-13 14:24:28 -0500

    top stories load

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+// import logo from './logo.svg';
 import './App.css';
 import MyList from './component/MyList';
 import Notes from './component/Notes';
@@ -7,11 +8,50 @@ import TopStories from './component/TopStories';
 import Story from './component/Story';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      topStories: []
+    }
+  }
+
+  componentDidMount() {
+    this.getRequest();
+  }
+
+  getRequest() {
+    const nyTimesUrl = 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key={key}'
+    axios.get(nyTimesUrl)
+      .then((response) => {
+        // console.log(response);
+        const data = response.data.results;
+        console.log(data);
+        let topStories = [];
+          if(data) {
+            topStories = Object.keys(data).map((id) => {
+              const story = data[id];
+              console.log(story);
+              return {
+                id: id,
+                topArticleTitle: story.title
+              }
+             });
+
+          }
+        this.setState({ topStories });
+      })
+      .catch((error) => {
+       });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="Stories">
-          <TopStories />
+          <TopStories
+            id="test"
+            topStories={this.state.topStories}
+          />
           <Story />
         </div>
         <div className="myListNotes">
--- src/component/TopStories.js
+++ src/component/TopStories.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 
 class TopStories extends Component {
+
+
   render() {
-    return(
-      <h1>Top Stories</h1>
+    let storyList = this.props.topStories.map((story, i) => {
+      //console.log(story);
+      return (
+        <li key={i}>{story.topArticleTitle}</li>
+      );
+    });
+    return (
+      <div>
+        <h1>Top Stories</h1>
+        <ul>
+          {storyList}
+        </ul>
+      </div>
     );
   }
 }
